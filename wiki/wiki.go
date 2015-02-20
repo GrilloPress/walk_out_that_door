3dda7134809c6d86be4b5502c26e090dec63bf2b
GrilloPress <grillopress@gmail.com>
2015-02-20 15:49:40 +0000

    add comments to go file

--- wiki/wiki.go
+++ wiki/wiki.go
@@ -7,7 +7,7 @@ import (
 	
 )
 
-// like a class in a way. The pattern we model our page after
+// A representation of data. Mutable -- ie you can update it or change it. The pattern. Or model of our page
 
 type Page struct {
 	
@@ -16,12 +16,22 @@ type Page struct {
 	
 }
 
+// this creates a function called save -- it takes a reciever called p and a pointer to the Page struct/instance.
+// The file name is then given the name of the title p.Title and it is make into a markdown file with the extension of .md
+// it finally returns by called the ioutil utility of writing a file, injects the filename and body and sets the permissions (0600)
+
 func (p *Page) save() error {
 	
+
 	filename := p.Title + ".md"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 	
 }
+
+// This function lets you read file. It takes the parameters of title. It then turns that into the filename with the markdow extension
+// After this is takes the body and loads that through the ioutil utility
+// If there are any errors it returns nil and the error message
+// If all is good, it returns with the title and body and nil
 
 func loadPage(title string) (*Page, error) {
 	
@@ -35,6 +45,8 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 	
 }
+
+// this function which runs when the file is loaded up will create a file, save the body and title with the below values. Sve it. Then load the file via the title and print it to the screen
 
 func main() {
 	
