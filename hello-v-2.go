f4a873a14d700ab76cb936cbef1fe2f6879e6f3b
GrilloPress <grillopress@gmail.com>
2015-02-20 13:07:13 +0000

    add comments into source code to explain value of fmt import

--- hello-v-2.go
+++ hello-v-2.go
@@ -7,3 +7,5 @@ func main() {
 	fmt.Printf("Hello World! I have come to destroy you all \n")
 	
 }
+
+// the format or fmt package is similar to the <stdio.h> package which imports formatted IO functions... so essentially printf(); in C - practically, this enables the developer to safely include things into strings, like %s for injecting in a string
